4be3f455a5156a5e2e5270d654b8f9a0175b69a8
sandeep281186 <51852137+sandeep281186@users.noreply.github.com>
2021-04-15 21:21:07 +0530

    Add files via upload

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainForm mainFrom = new MainForm();
+            Application.Run(mainFrom);
+        }
+    }
+}
