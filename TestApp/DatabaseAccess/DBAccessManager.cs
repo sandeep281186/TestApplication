0c7d373edbb22938c45a016d93837852e2ab6a54
Amit <amit180889@gmail.com>
2021-06-19 20:28:43 +0530

    Nuget references issue - Fixed

--- TestApp/DatabaseAccess/DBAccessManager.cs
+++ TestApp/DatabaseAccess/DBAccessManager.cs
@@ -113,7 +113,7 @@ namespace DatabaseAccess
                 conn = null;
             }
         }
-
+            
         private void Connect()
         {
             conn = new SqliteConnection(connectionString);
@@ -193,27 +193,35 @@ namespace DatabaseAccess
         public DataTable LoadTurretData()
         {
             DataTable dt = new DataTable();
-           
+
             dt.Columns.Add("User");
             dt.Columns.Add("iTurret Logon Name");
             dt.Columns.Add("Key Type");
             dt.Columns.Add("Key Label");
             dt.Columns.Add("Page Name");
-            dt.Columns.Add("Page Number");
-            dt.Columns.Add("Key Index");
-            dt.Columns.Add("Key Page");
-            dt.Columns.Add("Policy Speaker");
-            dt.Columns.Add("Page Speaker Channel");
+            dt.Columns.Add("Page Number", typeof(int));
+            dt.Columns.Add("Key Index", typeof(int));
+            dt.Columns.Add("Key Page Policy");
+            dt.Columns.Add("Speaker Page");
+            dt.Columns.Add("Speaker Channel");
             dt.Columns.Add("Appearance Type");
             dt.Columns.Add("Directory Type");
             dt.Columns.Add("Address");
             dt.Columns.Add("Line Reference");
+            dt.Columns.Add("RecordID", typeof(int));
 
             try
             {
                 using (var command = conn.CreateCommand())
                 {
-                    command.CommandText = "Select * from iTurretData";
+                    //command.CommandText = "Select * from iTurretData";
+
+                    command.CommandText = "select * from iTurretDataModified UNION Select * from iTurretData where RecordID not in (Select RecordID from iTurretDataModified)";
+
+                    //SqlCommand cmd = new SqlCommand("", conn);
+                    //SqlDataAdapter sdAda = new SqlDataAdapter(cmd);
+                   
+
 
                     using (SqliteDataReader reader = command.ExecuteReader())
                     {
@@ -223,7 +231,7 @@ namespace DatabaseAccess
                                 reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3),
                                 reader.GetString(4), reader.GetString(5), reader.GetString(6), reader.GetString(7),
                                 reader.GetString(8), reader.GetString(9), reader.GetString(10), reader.GetString(11),
-                                reader.GetString(12), reader.GetString(13)});
+                                reader.GetString(12), reader.GetString(13), reader.GetString(14)});
                         }
                     }
                 }
@@ -234,6 +242,81 @@ namespace DatabaseAccess
             }
 
             return dt;
+        }
+
+        public bool UpdateDB(DataTable dtOrig, DataTable dtUpdated)
+        {
+            bool updated = false;
+            DataTable recordIds = new DataTable();
+            recordIds.Columns.Add("RecordID", typeof(int));
+
+            try
+            {
+                using (var command = conn.CreateCommand())
+                {
+                    command.CommandText = "Select RecordID from iTurretDataModified";
+
+                    using (SqliteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            recordIds.Rows.Add(new object[] {  reader.GetString(0)});
+                        }
+                    }
+                }
+
+                //DataTable dtDiff = dtUpdated.AsEnumerable().Except(dtOrig.AsEnumerable(), DataRowComparer.Default).CopyToDataTable();
+                DataTable dtDiff = dtUpdated.GetChanges(DataRowState.Modified);
+
+                using (var sqlCmd = conn.CreateCommand())
+                {
+                    foreach (DataRow dr in dtDiff.Rows)
+                    {
+                        if (recordIds.AsEnumerable().Where(x => (int)x.Field<object>("RecordID")==(int)dr["RecordID"]).ToList().Count > 0)
+                        {
+                            using (var command = conn.CreateCommand())
+                            {
+                                command.CommandText = "Update iTurretDataModified Set 'Key Label'='"+ dr["Key Label"]  + "'" + " where RecordID="+dr["RecordID"];
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                        else
+                        {
+                            string commandText = "INSERT INTO iTurretDataModified (" +
+                                   "'" + "USER" + "','" + "iTurret Logon Name" + "','" + "Key Type" + "','" + "Key Label" + "','" + "Page Name" + "','" + "Page Number" + "','" + "Key Index" +
+                                   "','" + "Key Page Policy" + "','" + "Speaker Page" + "','" + "Speaker Channel" + "','" + "Appearance Type" + "','" + "Directory Type" + "','" + "Address" +
+                                   "','" + "Line Reference" + "'," + "RecordID) VALUES ("
+                                   + "'" + dr["User"] + "',"
+                                   + "'" + dr["iTurret Logon Name"] + "',"
+                                   + "'" + dr["Key Type"] + "',"
+                                   + "'" + dr["Key Label"] + "',"
+                                   + "'" + dr["Page Name"] + "',"
+                                   + dr["Page Number"] + ","
+                                   + dr["Key Index"] + ","
+                                   + "'" + dr["Key Page Policy"] + "',"
+                                   + "'" + dr["Speaker Page"] + "',"
+                                   + "'" + dr["Speaker Channel"] + "',"
+                                   + "'" + dr["Appearance Type"] + "',"
+                                   + "'" + dr["Directory Type"] + "',"
+                                   + "'" + dr["Address"] + "',"
+                                   + "'" + dr["Line Reference"] + "',"  + dr["RecordID"] + ");";
+
+                                    using (var command = conn.CreateCommand())
+                                    {
+                                        command.CommandText = commandText;
+                                        command.ExecuteNonQuery();
+                                    }
+                          }
+                    }
+                    updated = true;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            return updated;
         }
     }
 }
